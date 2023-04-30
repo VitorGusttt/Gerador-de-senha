40ea49f3c7f742a4953eaac81ae5b4dec0b023bf
unknown <vittorbeatz@gmail.com>
2023-04-30 11:31:03 -0300

    Add input range and limit characters

--- js/apps.js
+++ js/apps.js
@@ -2,27 +2,34 @@
 const btnGerarSenha = document.querySelector("#gerarSenha");
 const textoSenha = document.querySelector("#textoSenha");
 const btnCopy = document.querySelector("#btnCopy");
+const rangeValue = document.querySelector("#rangeElementos");
+const spanValue = document.querySelector("#spanValue");
+
+
+//aqui eu mudo os valores do span com base no range
+rangeValue.addEventListener("input", () =>{
+    spanValue.innerHTML = `${rangeValue.value}`
+})
+
 
 //fazer uma funcao que faz senhas utilizando minusculos maiusculas @# e numeros
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS123456789!@#$";
-
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS123456789!@#$&*";
 function gerarSenha() {
     let senhaGerada ="";
 
-    for (i = 0 ; i < 10; i++){
+    for (i = 0 ; i < rangeValue.value; i++){
         senhaGerada += charset.charAt(Math.floor(Math.random() * charset.length))
     }
     textoSenha.innerHTML = senhaGerada
 };
 
 
+
 //funcao que copia para a area de transferencia
 function copiar () {
     let copysenha = document.querySelector('#textoSenha')
     navigator.clipboard.writeText(copysenha.innerHTML)
-    alert("copied password")
-
-
+    alert(`copied password: ${copysenha.innerHTML}`)
 };
 
 //escuta o click e raliza funcao
